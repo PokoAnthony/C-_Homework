68ba9cd4fb951e6ca5dd26bad837d2421a389d4f
Anton <Pokorskiy@yandex.ru>
2022-11-28 21:14:08 +0300

    Добавил решение задач № 25, 27, 29

---
+++ Seminar_4/Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Не используя Math.Pow
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int natural = 1;
+
+for (int i = 1; i <= b; i++)
+{
+    natural = natural * a;
+}
+Console.WriteLine();
+Console.WriteLine("{0} ^ {1}  = {2}", a, b, natural);
---
+++ Seminar_4/Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+string str = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < str.Length; i++)
+{
+    int number = int.Parse(str[i].ToString());
+    sum = sum + number;
+}
+
+Console.WriteLine("Сумма цифр в числе = " + sum);
